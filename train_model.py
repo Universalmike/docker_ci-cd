05c4f409bbfec46b9e8a1d2898d4ea2f3e50ed8c
Universalmike <150071045+Universalmike@users.noreply.github.com>
2025-06-22 21:46:43 +0100

    Add files via upload

---
+++ train_model.py
@@ -0,0 +1,12 @@
+# train_model.py
+from sklearn.datasets import load_iris
+from sklearn.ensemble import RandomForestClassifier
+import joblib
+
+X, y = load_iris(return_X_y=True)
+model = RandomForestClassifier()
+model.fit(X, y)
+
+joblib.dump(model, 'app/iris.pkl')
+
+print("done")
