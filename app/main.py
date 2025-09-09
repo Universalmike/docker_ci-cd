aa44c6b01f87cef8f3b5eabe81deb2c17cb6809b
Universalmike <150071045+Universalmike@users.noreply.github.com>
2025-09-09 22:58:20 +0100

    Update main.py

--- app/main.py
+++ app/main.py
@@ -8,10 +8,11 @@ class IrisInput(BaseModel):
     features: list
 
 app = FastAPI()
-model = joblib.load("app/iris.pkl")
+model = joblib.load("models/iris.pkl")
 
 @app.post("/predict")
 def predict(data: IrisInput):
     input_data = np.array(data.features).reshape(1, -1)
     prediction = model.predict(input_data)
     return {"prediction": int(prediction[0])}
+
