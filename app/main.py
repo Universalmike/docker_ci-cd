c663f7c4076cec8513d5eae96bca394f21bb29c1
Universalmike <150071045+Universalmike@users.noreply.github.com>
2025-09-12 07:44:53 +0100

    Added a root message

--- app/main.py
+++ app/main.py
@@ -10,9 +10,14 @@ class IrisInput(BaseModel):
 app = FastAPI()
 model = joblib.load("models/iris.pkl")
 
+@app.get("/")
+def read_root():
+    return {"message": "FastAPI app is running!"}
+
 @app.post("/predict")
 def predict(data: IrisInput):
     input_data = np.array(data.features).reshape(1, -1)
     prediction = model.predict(input_data)
     return {"prediction": int(prediction[0])}
+
 
